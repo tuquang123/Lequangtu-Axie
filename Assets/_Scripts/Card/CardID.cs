b8cee032b3ec5cc550ca2250f367d1d87f5903c6
hovhoanganh <hovanhoanganh1@gmail.com>
2022-08-27 17:13:16 +0700

    update scriptableobject

--- Assets/_Scripts/Card/CardID.cs
+++ Assets/_Scripts/Card/CardID.cs
@@ -1,8 +1,15 @@
 public enum CardID
 {
-    A = 0,
-    B = 1,
-    C = 2,
+    Bumm = 0,
+    Dame = 1,
+    Hp = 2,
+   Land = 3,
+    Light = 4,
+    Rain = 5,
+    Shield = 6,
+    Thunder = 7,
+    Water = 8,
+    Winter = 9
 }
 
 
