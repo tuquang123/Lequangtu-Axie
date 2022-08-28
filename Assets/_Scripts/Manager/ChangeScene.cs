4cdce7075630507f4fbda75c4d747853fdb29259
IrrationaL <kjbinben@gmail.com>
2022-08-28 07:23:37 +0700

    Load Scene

--- Assets/_Scripts/Manager/ChangeScene.cs
+++ Assets/_Scripts/Manager/ChangeScene.cs
@@ -7,8 +7,26 @@ namespace _Scripts
 {
     public class ChangeScene : MonoBehaviour
     {
+        public static int gameLevel;
         public GameObject player;
-    
+
+        public void SetGameLevel(int level)
+        {
+            gameLevel = level;
+        }
+        
+        public void LoadIntoSelectCardScene()
+        {
+            player.SetActive(false);
+            g2(1);
+        }
+
+        public void LoadIntoLevelScene()
+        {
+            player.SetActive(false);
+            g2(gameLevel+1);
+        }
+        
         public void Change1(int index)
         {
             player.SetActive(false);
